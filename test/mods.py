b3222d6883c0cd3ec06f82e2359cd9b7211dddde
Ben Cail <benjamin_cail@brown.edu>
2012-10-04 11:11:41 -0400

    MODS - add a test for personal names.

--- test/mods.py
+++ test/mods.py
@@ -95,6 +95,12 @@ class ModsReadWrite(unittest.TestCase):
     def test_sample_mods(self):
         loaded = load_xmlobject_from_string(SAMPLE_MODS, Mods)
         self.assertEqual(loaded.title, 'Time Travels: Metalepsis and Modernist Poetry')
+        #test personal names
+        personal_names = loaded.personal_name
+        personal_name_list = ['Ben-Merre, David N.', 'Blasing, Mutlu', 'Katz, Tamar', 'Keach, William', 'Smith, Barbara']
+        self.assertEqual(len(personal_names), 5)
+        for i in range(5):
+            self.assertTrue(personal_names[i].namePart in personal_name_list)
         self.assertEqual(loaded.typeOfResource, 'text')
         self.assertEqual(loaded.genre, 'theses')
 
