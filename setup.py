2cbc85a5c1873f5c15aad9c9f0421a8b3536eb91
Ben Cail <benjamin_cail@brown.edu>
2013-03-19 14:25:43 -0400

    bump version to 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
                              'templates/*.*']},
