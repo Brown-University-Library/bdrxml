55ed8f811e7c544933b692f9a965570a7d383526
Ben Cail <benjamin_cail@brown.edu>
2013-06-21 12:04:20 -0400

    override origin_info so we can have dateOther fields

--- bdrxml/mods.py
+++ bdrxml/mods.py
@@ -1,12 +1,23 @@
 from eulxml.xmlmap import mods as eulmods
 from eulxml.xmlmap import StringField as SF
-from eulxml.xmlmap import NodeListField
+from eulxml.xmlmap import NodeListField, NodeField
 
 MODS_NAMESPACE = eulmods.MODS_NAMESPACE
 
 
 class TitleInfo(eulmods.TitleInfo):
     label  = SF('@displayLabel')
+
+
+class DateOther(eulmods.Date):
+    ROOT_NAME = 'dateOther'
+    type = SF('@type')
+
+
+class OriginInfo(eulmods.OriginInfo):
+    other = NodeListField('mods:dateOther', DateOther,
+        verbose_name='Date Other',
+        help_text='Other Date')
 
 
 class Mods(eulmods.MODSv34):
@@ -18,6 +29,8 @@ class Mods(eulmods.MODSv34):
     id = SF('@ID')
     #override eulxml title_info, because we need to support multiple titles
     title_info = NodeListField('mods:titleInfo', TitleInfo)
+    #override eulxml origin_info, because we need to support dateOther
+    origin_info = NodeField('mods:originInfo', OriginInfo)
 
 
 def make_mods():
