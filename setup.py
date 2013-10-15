9a532a1739551d26ff5f9250b18e35af75ed719a
Ben Cail <benjamin_cail@brown.edu>
2013-10-15 11:05:09 -0400

    update version number to 0.6-dev, since we just tagged v0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.5',
+    version='0.6-dev',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
                              'templates/*.*']},
