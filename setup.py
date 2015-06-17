2d73228fe7631e511d2f733b690cfb9ee0ebc097
Ben Cail <benjamin_cail@brown.edu>
2015-06-17 10:17:07 -0400

    set version to 0.7-dev for new development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.6',
+    version='0.7-dev',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
                              'templates/*.*']},
