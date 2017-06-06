f5a4fc22e6c96fd0d235e18237b39af4898678a4
Ben Cail <benjamin_cail@brown.edu>
2017-06-06 14:56:55 -0400

    update version to 0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.8a1',
+    version='0.8',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
                              'templates/*.*']},
