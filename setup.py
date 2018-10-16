d8dca66ceb1fe254ca4bc29dcf4d3e5f81273ca4
bcail <benjamin_cail@brown.edu>
2018-10-16 16:20:06 -0400

    add ply==3.8 to requirements, set eulxml to 1.1.3, and update version to 0.9a1 (#90)

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.8.1',
+    version='0.9a1',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
                              'templates/*.*']},
     install_requires=[
-        'eulxml>=1.0.1',
+        'eulxml==1.1.3',
+        'ply==3.8',
     ]
 )
