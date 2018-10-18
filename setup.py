43a8eea3301fe138548702d6b78ee0ddedf7786b
Ben Cail <benjamin_cail@brown.edu>
2018-10-18 11:18:17 -0400

    don't try to package test/data - shouldn't have been working anyway

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(name='bdrxml',
     version='0.9',
     packages=find_packages(),
-    package_data={'bdrxml': ['test/data/*.*',
-                             'schemas/*.*']},
+    package_data={'bdrxml': ['schemas/*.*']},
     install_requires=[
         'eulxml==1.1.3',
         'ply==3.8',
