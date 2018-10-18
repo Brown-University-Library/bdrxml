1bc69977a32c5eb6c196b4b2ad165bb4c5a15c60
Ben Cail <benjamin_cail@brown.edu>
2018-10-18 10:53:45 -0400

    install schemas with package. Set version to 0.9

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='0.9a1',
+    version='0.9',
     packages=find_packages(),
     package_data={'bdrxml': ['test/data/*.*',
-                             'templates/*.*']},
+                             'schemas/*.*']},
     install_requires=[
         'eulxml==1.1.3',
         'ply==3.8',
