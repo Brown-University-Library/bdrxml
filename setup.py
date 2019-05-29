2146a616e8f52908424f6f5c05ed3b961c542b9e
Ben Cail <benjamin_cail@brown.edu>
2019-05-29 11:47:36 -0400

    add license and setup.py classifiers

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(name='bdrxml',
     install_requires=[
         'eulxml==1.1.3',
         'ply==3.8',
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
 
