c150de45fa787b477125b95ada6aa176859aa3c6
Ben Cail <benjamin_cail@brown.edu>
2019-05-30 10:43:37 -0400

    update version to 1.1a1 for development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='1.0',
+    version='1.1a1',
     url='https://github.com/Brown-University-Library/bdrxml',
     author='Brown University Library',
     author_email='bdr@brown.edu',
