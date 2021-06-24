b5d1c667a3d35cb6ad52298cba5ccc8b7cd9b597
Ben Cail <benjamin_cail@brown.edu>
2021-06-24 09:08:51 -0400

    version 1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrxml',
-    version='1.2',
+    version='1.3',
     url='https://github.com/Brown-University-Library/bdrxml',
     author='Brown University Library',
     author_email='bdr@brown.edu',
